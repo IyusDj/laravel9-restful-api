0bc25655047cc2559e4e0d458e5d65081d8b8cc4
IyusDj <iyusdjruswana@gmail.com>
2022-04-11 20:38:37 +0700

    Create data to database

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -16,5 +17,28 @@ class ProductController extends Controller
 
         // Return collection of product as a resource
         return new ProductResource(true, 'List Data Product', $product);
+    }
+
+    public function store(Request $request)
+    {
+        // Define validation rules
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required',
+            'price'     => 'required',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Create data product
+        $product = Product::create([
+            'name'      => $request->name,
+            'price'     => $request->price,
+        ]);
+
+        //return response
+        return new ProductResource(true, ' Success Add Data Product!', $product);
     }
 }
